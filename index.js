3c762a203e858ef2f9d94ffce618dddb30f9857a
Greg Connell <greg.connell@ff0000.com>
2017-11-12 01:51:03 -0800

    adding polyfills dependency

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { Async } from 'ad-control'
 window.Async = Async // temp hack to get Async module back into the global scope
-import * as scope from './lib/scope'
+import { customEvent } from 'ad-polyfills'
+
+import * as scope from './lib/scope.js'
 import * as preloader from './lib/preloader.js'
 import * as payloader from './lib/payloader.js'
-import './lib/polyfills.js'
+
 
 // NOTE: This class expects window-scoped methods to exist:
 
