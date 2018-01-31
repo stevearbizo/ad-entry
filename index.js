1636f43e425c799c0c65c8f4f6d849a4f5db60db
Greg Connell <greg.connell@ff0000.com>
2018-01-30 16:18:55 -0800

    refactors Async out of Preloader

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-import Async from 'ad-control/lib/Async'
-window.Async = Async // temp hack to get Async module back into the global scope
 import 'ad-polyfills'
-
 import * as scope from './lib/scope.js'
 import * as preloader from './lib/preloader.js'
 import * as payloader from './lib/payloader.js'
