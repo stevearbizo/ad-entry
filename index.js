add4039041b39260beed597b7ee7f34d290cdaec
Greg Connell <greg.connell@gregc-mbp.ff0000.com>
2018-01-30 18:10:51 -0800

    big refactor pass

--- index.js
+++ index.js
@@ -4,21 +4,39 @@ import * as preloader from './lib/preloader.js'
 import * as payloader from './lib/payloader.js'
 
 // NOTE: This class expects window-scoped methods to exist:
-// prepare index
-window.prepareIndex(scope, preloader, payloader)
+window
+	// prepare vendor onload (Network.js)
+	.prepareIndex()
 
-// prepare network
-window.prepareNetworkExit()
-
-// make ad clickable ASAP
-window.makeAuxClickable()
-
-// payloader
-payloader
-	.execute()
+	// prepare scope
 	.then(() => {
-		window.onImpression()
+		return scope.prepare().then(() => {
+			// misc
+			window.prepareAdParamsMisc()
+
+			// prepare network
+			window.prepareNetworkExit()
+
+			// make ad clickable ASAP
+			window.makeAuxClickable()
+		})
 	})
+
+	// prepare preload
+	.then(() => {
+		return preloader.prepare().then(() => {
+			window.preparePreloadMisc()
+		})
+	})
+
+	// prepare payload
+	.then(() => {
+		return payloader.execute().then(fbaContent => {
+			window.onImpression(fbaContent)
+		})
+	})
+
 	.catch(err => {
+		console.error(err)
 		window.failAd()
 	})
