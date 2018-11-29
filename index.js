d4658c8a03fd51ac70b5994f2ae544c70991c234
Greg Connell <greg.connell@ff0000.com>
2018-11-28 16:17:18 -0800

    makes `politeLoadAfter` a cummulative delay that includes bootstrapping

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 import 'ad-polyfills'
 import * as scope from './lib/scope.js'
+import * as polite from './lib/polite.js'
 import * as preloader from './lib/preloader.js'
 import * as payloader from './lib/payloader.js'
 
-// NOTE: This class expects window-scoped methods to exist:
-window
+/*
+ * 	NOTE: 
+ *	
+ *	This class expects window-scoped methods to exist
+ *
+ */
+
+polite
+	.prepare(window.adParams.politeLoadAfter)
+
 	// prepare vendor onload (Network.js)
-	.prepareIndex()
+	.then(() => window.prepareIndex()
 
 	// prepare scope
-	.then(() => {
-		return scope.prepare().then(() => {
-			// misc
-			window.prepareAdParamsMisc()
+	.then(() => scope.prepare())
 
-			// prepare network
-			window.prepareNetworkExit()
+	// misc index control
+	.then(() => window.prepareAdParamsMisc())
 
-			// DEPRECATED 4/19/18 - this now happens on window.preparePreloadMisc()
-			// but must keep for older ads so the failover is clickable!
-			if (!window.isVersionOrNewer || !window.isVersionOrNewer('0.1.2')) {
-				window.makeAuxClickable()
-			}
-		})
-	})
+	// prepare network
+	.then(() => window.prepareNetworkExit())
 
-	// prepare preload
-	.then(() => {
-		return preloader.prepare().then(() => {
-			window.preparePreloadMisc()
-		})
-	})
+	// prepare preloader
+	.then(() => preloader.prepare())
+	.then(() => window.preparePreloadMisc())
+
+	// finish polite timeout
+	.then(() => polite.resolveDelay())
 
 	// prepare payload
-	.then(() => {
-		return payloader.execute().then(fbaContent => {
-			window.onImpression(fbaContent)
-		})
-	})
+	.then(() => payloader.execute())
+
+	// launch polite
+	.then(fbaContent => window.onImpression(fbaContent))
 
 	.catch(err => {
 		console.error(err)
